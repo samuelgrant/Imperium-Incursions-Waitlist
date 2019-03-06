a14e54b6bac612ee46a88a74f6bd6117f67bbc5d
jacksos101 <jacksos101@gmail.com>
2019-03-06 19:54:17 +1300

    Configure database context

---
+++ Src/Data/AppDataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Imperium_Incursions_Waitlist.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imperium_Incursions_Waitlist.Data
+{
+    public class AppDataContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Ban> Bans { get; set; }
+        public DbSet<Pilot> Pilots { get; set; }
+
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
