b7619f71321d071fa5fdffce50c1056306a4e5c4
jacksos101 <jacksos101@gmail.com>
2019-05-03 16:56:23 +1200

    Add models for corps and alliances

---
+++ Src/Models/Alliance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imperium_Incursions_Waitlist.Models
+{
+    public class Alliance
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        // Navigation properties
+
+        public Corporation Corporation { get; set; }
+    }
+}
---
+++ Src/Models/Corporation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Imperium_Incursions_Waitlist.Models
+{
+    public class Corporation
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public int AllianceId { get; set; }
+
+        public string Name { get; set; }
+
+        // Navigation properties
+
+        public Alliance Alliance { get; set; }
+        public ICollection<Pilot> Pilots { get; set; }
+    }
+}
