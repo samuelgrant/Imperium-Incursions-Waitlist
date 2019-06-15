219c3f9f148a9f85b7df6c41b7a4d899efff11ab
Samuel <samueljegrant@outlook.com>
2019-06-16 00:28:09 +1200

    Fleet at a glance pt2
    Fleet at a glance now works:
    - The ship icon, name and number of pilots now renders.
    - Mousing over the name will bring up a tooltip of pilots
    - Filters "Fleet" and "Support" work, but the tabs and callbacks are static
    
    **TODO**
    - Make the tabs, and numbers for tabs dynamic

--- Src/Resources/js/React/Components/FleetGlance.js
+++ Src/Resources/js/React/Components/FleetGlance.js
@@ -1,6 +1,14 @@
 ﻿import React, { Component } from 'react';
+import ReactTooltip from 'react-tooltip'
 
 export default class FleetGlance extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedQueue: 0
+        };
+    }
+
     getPilots() {
         if (this.props.members && this.props.members.pilots)
             return this.props.members.pilots;
@@ -10,6 +18,9 @@ export default class FleetGlance extends Component {
 
     getComp(pilots) {
         var dict = new Object();
+
+        if (pilots == null)//>>> Can I look up pilots here instead of having it's own method, and wrap the whole dict logic inside of an if assuming we have pilots?
+            return null;
 
         for (let i = 0; i < pilots.length; i++) {
             let ship = pilots[i].ship;
@@ -29,25 +40,41 @@ export default class FleetGlance extends Component {
             Object.keys(dict).forEach(function (key) {
                 dict[key].pilots.sort();
             });
-
-
-            //>> map object into an array if queue ID matches id in state
         }
-
 
         return dict;
     }
 
-    render() {
-        if (this.getPilots())
-            console.log(this.getComp(this.getPilots()))
-        //console.log(this.getComp(this.getPilots()))
-        
+    getFilteredComp() {
+        let filteredComp = [];
+        let comp = this.getComp(this.getPilots());
+
+        if (comp == null)
+            return null;
+
+        var xxx = Object.keys(comp).map(function (key) {
+            return [comp[key]];
+        });
+
+        for (let i = 0; i < xxx.length; i++) {
+            if (this.state.selectedQueue == 0 || xxx[i][0].queue.id == this.state.selectedQueue) {
+                filteredComp.push(xxx[i]);
+            }
+        }
+
+        return filteredComp;
+    }
+
+    setFilter(filterId) {
+        this.setState({ selectedQueue: filterId });
+    }
+
+    render() {       
         return (
             <div className="row">
-                <GlanceMenu />
+                <GlanceMenu updateFilter={this.setFilter.bind(this)}/>
 
-                <GlanceComp />
+                <GlanceComp comp={this.getFilteredComp()} />
             </div>
         )
     }
@@ -55,7 +82,31 @@ export default class FleetGlance extends Component {
 
 export class GlanceComp extends Component {
     render() {
-        return (<span>Glance Here</span>);
+        let ships;
+        if (this.props.comp) {
+            ships = this.props.comp.map((ship) => {
+                let pilots = ship[0].pilots.join("<br>");
+
+
+                return (
+                    <div className="col-lg-6 p-3">
+                        <img class="rounded pr-3" src={`https://image.eveonline.com/Render/${ship[0].id}_32.png`} alt={ship[0].name} />
+                        <p className="d-inline pr-3" data-tip={pilots} data-multiline="true">{ship[0].name}</p>
+                        <span className="badge badge-warning">{ship[0].pilots.length}</span>
+                        <ReactTooltip />
+                    </div>
+                )
+            })
+        }
+
+
+        return (
+            <div className="col-12">
+                <div className="row">
+                    {ships}
+                </div>
+            </div>
+        );
     }
 }
 
@@ -65,7 +116,7 @@ export class GlanceMenu extends Component {
             <div className="col-12">
                 <ul class="nav nav-tabs nav-justified">
                     <li className="nav-item">
-                        <a role="tab" data-toggle="tab" className="nav-link active" href="#tab-1">Fleet</a>
+                        <a role="tab" data-toggle="tab" className="nav-link active" href="#tab-1" onClick={this.props.updateFilter.bind(this, 0)}>Fleet</a>
                         <span className="badge badge-warning">42</span>
                     </li>
                     <li className="nav-item">
@@ -85,7 +136,7 @@ export class GlanceMenu extends Component {
                         <span className="badge badge-dark">42</span>
                     </li>
                     <li class="nav-item">
-                        <a role="tab" data-toggle="tab" className="nav-link" href="#tab-3">Support</a>
+                        <a role="tab" data-toggle="tab" className="nav-link" href="#tab-3" onClick={this.props.updateFilter.bind(this, 5)}>Support</a>
                         <span className="badge badge-dark">42</span>
                     </li>
                 </ul>
